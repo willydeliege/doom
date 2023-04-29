9cb6ff755b898858909868bbb137734a2767ff86
Frédéric Willem <frederic.willem@gmail.com>
2023-04-29 08:17:29 +0200

    remove unneeded files

--- org_comfig.el
+++
@@ -1,6 +0,0 @@
-(setq browse-url-browser-function 'browse-url-generic)
-(setq browse-url-generic-program  "google-chrome-stable")
-;; Global settings (defaults)
-(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-      doom-themes-enable-italic t) ; if nil, italics is universally disabled
-(load-theme 'ef-deuteranopia-dark t)
