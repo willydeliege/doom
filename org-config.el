7f36da8e004162b12379c86f768698bd4fc6922e
Frédéric Willem <frederic.willem@gmail.com>
2023-05-03 07:31:18 +0200

    update org-roam config for project management

--- org-config.el
+++ org-config.el
@@ -298,36 +298,131 @@
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
 (setq org-refile-use-outline-path 'file)
 (setq org-refile-allow-creating-parent-nodes 'confirm)
+;; Org-roam config
+(with-eval-after-load 'org-roam
+  ;; Roam is always one level deep in my org-directory
+  (setq org-id-link-to-org-use-id t)
+  (setq org-roam-completion-system 'helm)
+  (add-to-list 'display-buffer-alist
+               '(("\\*org-roam\\*"
+                  (display-buffer-in-direction)
+                  (direction . right)
+                  (window-width . 0.33)
+                  (window-height . fit-window-to-buffer))))
+  (setq org-roam-capture-templates
+        '(("d" "default" plain "%?"
+           :immediate-finish t
+           :if-new (file+head "${slug}.org"
+                              "#+TITLE: ${title}\n#+hugo_lastmod: Time-stamp: <>\n\n")
+           :unnarrowed t)
+          ("t" "temp" plain "%?"
+           :if-new(file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                             "#+TITLE: ${title}\n#+hugo_lastmod: Time-stamp: <>\n\n")
+           :immediate-finish t
+           :unnarrowed t)
+          ("p" "private" plain "%?"
+           :if-new (file+head "${slug}-private.org"
+                              "#+TITLE: ${title}\n")
+           :immediate-finish t
+           :unnarrowed t)))
+           (defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (push arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
 
-  (with-eval-after-load 'org-roam
-    ;; Roam is always one level deep in my org-directory
-    (setq org-id-link-to-org-use-id t)
-    (setq org-roam-completion-system 'helm)
-    (add-to-list 'display-buffer-alist
-                 '(("\\*org-roam\\*"
-                    (display-buffer-in-direction)
-                    (direction . right)
-                    (window-width . 0.33)
-                    (window-height . fit-window-to-buffer))))
-    (setq org-roam-capture-templates
-          '(("d" "default" plain "%?"
-             :immediate-finish t
-             :if-new (file+head "${slug}.org"
-                                "#+TITLE: ${title}\n#+hugo_lastmod: Time-stamp: <>\n\n")
-             :unnarrowed t)
-            ("t" "temp" plain "%?"
-             :if-new(file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                               "#+TITLE: ${title}\n#+hugo_lastmod: Time-stamp: <>\n\n")
-             :immediate-finish t
-             :unnarrowed t)
-            ("p" "private" plain "%?"
-             :if-new (file+head "${slug}-private.org"
-                                "#+TITLE: ${title}\n")
-             :immediate-finish t
-             :unnarrowed t)))
-    (org-roam-db-autosync-mode)
-    )
-  (setq org-id-extra-files (org-roam--list-files org-roam-directory))
+(defun my/org-roam-filter-by-tag (tag-name)
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
+  ;; add inbox)
+
+(defun my/org-roam-list-notes-by-tag (tag-name)
+  (mapcar #'org-roam-node-file
+          (seq-filter
+           (my/org-roam-filter-by-tag tag-name)
+           (org-roam-node-list))))
+
+(defun my/org-roam-refresh-agenda-list ()
+  (interactive)
+  (setq org-agenda-files (my/org-roam-list-notes-by-tag "Project")))
+
+;; Build the agenda list the first time for the session
+(my/org-roam-refresh-agenda-list)
+
+(defun my/org-roam-project-finalize-hook ()
+  "Adds the captured project file to `org-agenda-files' if the
+capture was not aborted."
+  ;; Remove the hook since it was added temporarily
+  (remove-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Add project file to the agenda list if the capture was confirmed
+  (unless org-note-abort
+    (with-current-buffer (org-capture-get :buffer)
+      (add-to-list 'org-agenda-files (buffer-file-name)))))
+
+(defun my/org-roam-find-project ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Select a project file to open, creating it if necessary
+  (org-roam-node-find
+   nil
+   nil
+   (my/org-roam-filter-by-tag "Project")
+   nil
+   :templates
+   '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
+      :unnarrowed t))))
+
+(defun my/org-roam-capture-inbox ()
+  (interactive)
+  (org-roam-capture- :node (org-roam-node-create)
+                     :templates '(("i" "inbox" plain "* %?"
+                                  :if-new (file+head "Inbox.org" "#+title: Inbox\n")))))
+
+(defun my/org-roam-capture-task ()
+  (interactive)
+  ;; Add the project file to the agenda after capture is finished
+  (add-hook 'org-capture-after-finalize-hook #'my/org-roam-project-finalize-hook)
+
+  ;; Capture the new task, creating the project file if necessary
+  (org-roam-capture- :node (org-roam-node-read
+                            nil
+                            (my/org-roam-filter-by-tag "Project"))
+                     :templates '(("p" "project" plain "** TODO %?"
+                                   :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                          "#+title: ${title}\n#+category: ${title}\n#+filetags: Project"
+                                                          ("Tasks"))))))
+
+(defun my/org-roam-copy-todo-to-today ()
+  (interactive)
+  (let ((org-refile-keep t) ;; Set this to nil to delete the original!
+        (org-roam-dailies-capture-templates
+          '(("t" "tasks" entry "%?"
+             :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+        (org-after-refile-insert-hook #'save-buffer)
+        today-file
+        pos)
+    (save-window-excursion
+      (org-roam-dailies--capture (current-time) t)
+      (setq today-file (buffer-file-name))
+      (setq pos (point)))
+
+    ;; Only refile if the target file is different than the current file
+    (unless (equal (file-truename today-file)
+                   (file-truename (buffer-file-name)))
+      (org-refile nil nil (list "Tasks" today-file nil pos)))))
+
+(add-to-list 'org-after-todo-state-change-hook
+             (lambda ()
+               (when (equal org-state "DONE")
+                 (my/org-roam-copy-todo-to-today))))
+  (org-roam-db-autosync-mode)
+  )
+(setq org-id-extra-files (org-roam--list-files org-roam-directory))
 
 (defun my/caldav-sync-perso ()
   "Sync my local calendar in ~/org/calendar.org with my remote calendar"
@@ -355,11 +450,22 @@
   ;; use imagemagick, if available
   (when (fboundp 'imagemagick-register-types)
     (imagemagick-register-types))
-  (setq mu4e-update-interval 60)
+  (setq mu4e-headers-unread-mark    '("u" . "📩 "))
+  (setq mu4e-headers-draft-mark     '("D" . "🚧 "))
+  (setq mu4e-headers-flagged-mark   '("F" . "🚩 "))
+  (setq mu4e-headers-new-mark       '("N" . "✨ "))
+  (setq mu4e-headers-passed-mark    '("P" . "↪ "))
+  (setq mu4e-headers-replied-mark   '("R" . "↩ "))
+  (setq mu4e-headers-seen-mark      '("S" . " "))
+  (setq mu4e-headers-trashed-mark   '("T" . "🗑️"))
+  (setq mu4e-headers-attach-mark    '("a" . "📎 "))
+  (setq mu4e-headers-encrypted-mark '("x" . "🔑 "))
+  (setq mu4e-headers-signed-mark    '("s" . "🖊 ")) (setq mu4e-update-interval 60)
   (setq mu4e-maildir-shortcuts
         '( (:maildir "/INBOX" :key ?i)
-           (:maildir "/[Gmail]/Sent Mail"  :key ?s)
+           (:maildir "/[Gmail]/Sent Mail"  :key ?S)
            (:maildir "/[Gmail]/Trash" :key ?t)
+           (:maildir "/[Gmail]/Starred" :key ?s)
            (:maildir "/[Gmail]/All Mail"   :key ?a))))
 
 (with-eval-after-load "ispell"
