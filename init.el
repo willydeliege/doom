6cb7bf5596ca49058e5a077db5c21b479c8a88f0
Frédéric Willem <frederic.willem@gmail.com>
2023-05-18 19:00:15 +0200

    updates modules used

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
        (lsp +peek +lsp-ui)
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
@@ -147,7 +147,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org   +journal +pretty +pomodoro +roam2)               ; organize your plain life in plain text
+       (org   +contacts +journal +pandoc +pretty +pomodoro +roam2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -170,7 +170,7 @@
        yaml              ; JSON, but readable
 
        :email
-       (mu4e +org )
+       (mu4e +gmail +org)
        ;; (notmuch +org)
        ;;(wanderlust +gmail)
 
